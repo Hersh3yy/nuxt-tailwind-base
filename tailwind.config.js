9d23d84389c4afc793cfd612db2c754331f71c2b
HirenBudhAir <hirenbudhrani@gmail.com>
2023-10-13 02:39:44 +0200

    fix types, linting, etc

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./components/**/*.{js,vue,ts,html}",
+    "./layouts/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    './pages/**/*.{html,js,vue}',
+    "./app.vue",
+  ],
   theme: {
     extend: {},
   },
